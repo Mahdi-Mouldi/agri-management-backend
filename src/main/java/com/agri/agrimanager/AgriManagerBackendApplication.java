3e9a137846d1aea74980a7fe190b3192555069aa
mahdi <mahdi2082004@gmail.com>
2026-02-11 10:59:54 +0100

    Initial Spring Boot project setup

---
+++ src/main/java/com/agri/agrimanager/AgriManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.agri.agrimanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriManagerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgriManagerBackendApplication.class, args);
+    }
+
+}
