5fe05a2906e7aa37f1f8953a3e01a70bf3f8efcf
mahdi <mahdi2082004@gmail.com>
2026-02-11 10:59:54 +0100

    Initial Spring Boot project setup

---
+++ src/test/java/com/agri/agrimanager/AgriManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agri.agrimanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriManagerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
