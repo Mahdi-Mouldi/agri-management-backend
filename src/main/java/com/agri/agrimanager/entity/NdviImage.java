9a762f052f4d81563d196c0f7b85ae8ec786f99e
Mahdi-Mouldi <146773043+Mahdi-Mouldi@users.noreply.github.com>
2026-02-20 12:48:47 +0100

    adding NdviImage entity

---
+++ src/main/java/com/agri/agrimanager/entity/NdviImage.java
@@ -0,0 +1,52 @@
+package com.agri.agrimanager.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class NdviImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @ManyToOne
+    @JoinColumn(name ="parcelle_id", nullable=false)
+    private Parcelle parcelle;
+    // La parcelle agricole à laquelle cette image NDVI correspond
+    @Column(nullable = false)
+    private String imageUrl;
+    // L'URL de l'image NDVI récupérée depuis l'API (ex: Agromonitoring)
+    private double ndviMean;// La valeur moyenne du NDVI sur la parcelle (entre 0.0 et 1.0)
+    private double ndviMax;
+    private double ndviMin;
+    private double ndviStd;
+    // L'écart-type des valeurs NDVI sur la parcelle (mesure de variation)
+
+    @Column(nullable = false)
+    private LocalDate imageDate; // date de creation de l'image
+
+    private String sattelite;
+    // Nom du satellite qui a pris l'image ("Sentinel-2", "Landsat-8", etc.)
+
+    private Integer cloudCoverage;
+    // Pourcentage de nuages sur l'image (0 = pas de nuages, 100 = tout couvert)
+
+    @Column(name = "agromonitoring_polygon_id")
+    private String polygonId;
+    // L'identifiant du polygone Agromonitoring correspondant à cette parcelle
+
+    private LocalDate createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDate.now();
+    }
+}
