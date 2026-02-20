5a9fd6e69d7438b17633a1f48bba82c8c072ea04
Mahdi-Mouldi <146773043+Mahdi-Mouldi@users.noreply.github.com>
2026-02-20 12:58:39 +0100

    adding NdviImageDTO

--- src/main/java/com/agri/agrimanager/dto/NdviImageDTO.java
+++ src/main/java/com/agri/agrimanager/dto/NdviImageDTO.java
@@ -1,4 +1,25 @@
 package com.agri.agrimanager.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class NdviImageDTO {
+    private Long id;
+    private Long parcelleId;
+    private String imageUrl;
+    private double ndviMean;
+    private double ndviMax;
+    private double ndviMin;
+    private LocalDate imageDate;
+    private String sattelite;
+    private Integer cloudCoverage;
+
 }
