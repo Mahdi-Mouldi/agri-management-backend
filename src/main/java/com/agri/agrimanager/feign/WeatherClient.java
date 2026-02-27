fc857f93387b8ed8e4879dc3a4b0ba310612ad48
Mahdi-Mouldi <146773043+Mahdi-Mouldi@users.noreply.github.com>
2026-02-27 16:49:44 +0100

    feat(weather): implement OpenMeteo Feign client for weather forecast retrieval

--- src/main/java/com/agri/agrimanager/feign/WeatherClient.java
+++ src/main/java/com/agri/agrimanager/feign/WeatherClient.java
@@ -1,5 +1,21 @@
 package com.agri.agrimanager.feign;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClientProperties;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@FeignClient(name = "Open-Meteo", 
+        configuration = FeignConfig.class,
+        url = "${open-meteo.base.url}")
 public interface WeatherClient {
+    @GetMapping("/forecast")
+    String getWeatherForecast(
+            @RequestParam("latitude") double latitude,
+            @RequestParam("longitude") double longitude,
+            @RequestParam("daily") String daily,
+            @RequestParam("timezone") String timezone,
+            @RequestParam("start_date") String startDate,
+            @RequestParam("end_date") String endDate
+    );
 }
