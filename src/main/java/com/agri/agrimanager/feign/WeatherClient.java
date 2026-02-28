04434e89c3d060c2da49f223f4bf58923aac3682
Mahdi-Mouldi <146773043+Mahdi-Mouldi@users.noreply.github.com>
2026-02-28 14:35:35 +0100

    feat(weather): implement WeatherService with GeoJSON centroid extraction and OpenMeteo forecast integration

--- src/main/java/com/agri/agrimanager/feign/WeatherClient.java
+++ src/main/java/com/agri/agrimanager/feign/WeatherClient.java
@@ -5,15 +5,14 @@ import org.springframework.cloud.openfeign.FeignClientProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "Open-Meteo", 
-        configuration = FeignConfig.class,
+@FeignClient(name = "Open-Meteo",
         url = "${open-meteo.base.url}")
 public interface WeatherClient {
     @GetMapping("/forecast")
     String getWeatherForecast(
             @RequestParam("latitude") double latitude,
             @RequestParam("longitude") double longitude,
-            @RequestParam("daily") String daily,
+            @RequestParam("daily") String daily ,
             @RequestParam("timezone") String timezone,
             @RequestParam("start_date") String startDate,
             @RequestParam("end_date") String endDate
