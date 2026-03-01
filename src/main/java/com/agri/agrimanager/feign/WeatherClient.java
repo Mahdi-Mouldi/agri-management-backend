e549d1c1637c3613132b3d2c10048e6bad8f076a
Mahdi-Mouldi <146773043+Mahdi-Mouldi@users.noreply.github.com>
2026-03-01 15:35:41 +0100

    add weatherController for weatherservice

--- src/main/java/com/agri/agrimanager/feign/WeatherClient.java
+++ src/main/java/com/agri/agrimanager/feign/WeatherClient.java
@@ -1,7 +1,6 @@
 package com.agri.agrimanager.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.cloud.openfeign.FeignClientProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
